d04634acc0dbf3e1acd95c5bac95bd2c9f9656ea
Nathan Bolser <bolsern@aetna.com>
2021-09-01 11:40:18 -0600

    Add flash messaging

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,6 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
