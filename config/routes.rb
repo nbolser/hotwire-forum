146fe1afcf52e07123168ab15f9e574ba1b256a9
Nathan Bolser <bolsern@aetna.com>
2021-09-02 13:54:27 -0600

    Add Discussions#create

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'main#index'
 
-  resources :discussions, only: [:index]
+  resources :discussions, only: [:index, :new, :create]
 
   get 'main/index'
 end
