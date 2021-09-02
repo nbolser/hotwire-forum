5e72e32364c9f49e2ff9470bb5d9b5eb6537b216
Nathan Bolser <bolsern@aetna.com>
2021-09-02 14:32:41 -0600

    Add Discussions#destroy

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'main#index'
 
-  resources :discussions, only: [:index, :new, :create, :edit, :update]
+  resources :discussions
 
   get 'main/index'
 end
