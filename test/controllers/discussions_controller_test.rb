3a5f2c884ff86f9dd948de77ad9a31f411edbff1
Nathan Bolser <nbolser@gmail.com>
2021-09-09 22:16:30 -0600

    Remove test_unit

--- test/controllers/discussions_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class DiscussionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get discussions_index_url
-    assert_response :success
-  end
-end
