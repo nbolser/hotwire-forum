1ff6c8829ac513dcedaf80c2487cd43be46d2179
Nathan Bolser <bolsern@aetna.com>
2021-11-11 15:14:20 -0700

    Add Post#edit + realtime updates

--- app/controllers/discussions/posts_controller.rb
+++ app/controllers/discussions/posts_controller.rb
@@ -2,9 +2,24 @@ module Discussions
   class PostsController < ApplicationController
     before_action :authenticate_user!
     before_action :set_discussion
+    before_action :set_post, only: [:show, :edit, :update]
 
     def new
+    end
 
+    def edit
+
+    end
+
+    def update
+      respond_to do |format|
+        if @post.update(post_params)
+        format.html { redirect_to @post.discussion, notice: 'Post updated' }
+        else
+          format.turbo_stream
+          format.html { render :edit, status: :unprocessable_entity }
+        end
+      end
     end
 
     def create
@@ -24,6 +39,10 @@ module Discussions
 
     def set_discussion
       @discussion = Discussion.find(params[:discussion_id])
+    end
+
+    def set_post
+      @post = @discussion.posts.find(params[:id])
     end
 
     def post_params
